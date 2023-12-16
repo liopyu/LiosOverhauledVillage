519ac40ff9165fde30d85d7bc6e2055fa69c303f
Liopyu <mistakensound@gmail.com>
2023-12-16 09:08:27 -0500

    converted to 1.20.4

--- src/main/java/net/liopyu/lios_overhauled_villages/LiosOverhauledVillages.java
+++ src/main/java/net/liopyu/lios_overhauled_villages/LiosOverhauledVillages.java
@@ -3,6 +3,8 @@ package net.liopyu.lios_overhauled_villages;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,36 +14,55 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(lios_overhauled_villages.MOD_ID)
-public class lios_overhauled_villages
+@Mod(LiosOverhauledVillages.MOD_ID)
+public class LiosOverhauledVillages
 {
-
+    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "lios_overhauled_villages";
+    // Directly reference a slf4j logger
+    //private static final Logger LOGGER = LogUtils.getLogger();
 
-    private static final Logger LOGGER = LogUtils.getLogger();
-
-    public lios_overhauled_villages() {
+    public LiosOverhauledVillages()
+    {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
 
-
+        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
 
     }
 
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
 
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
 
         }
     }
